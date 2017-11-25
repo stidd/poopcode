393e5c166cac9a5387d4c9e4ab59a992034108e7
David Matheson <mathesond2@gmail.com>
2017-11-25 00:43:54 -0500

    added comments & random JS bg image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+/*
+See something missing? Brittle code? Something that could be done better? Check 
+out https://github.com/mathesond2/poopcode/ and find out how you can contribute!
+*/
+
 var redirectUrl = "http://www.popcode.org/";
 var timeBeforeRedirect = 3000;
 
 window.setTimeout(function() {
   window.location = redirectUrl;
 }, timeBeforeRedirect);
+
+// Big Hint: how could we find out the amount of images in the '/img' folder?
+var randomNumber = Math.floor(Math.random() * (8 - 1)) + 1;
+var htmlElement = document.querySelector('html');
+
+htmlElement.style.backgroundImage = 'url("./img/' + randomNumber + '.jpg")';
